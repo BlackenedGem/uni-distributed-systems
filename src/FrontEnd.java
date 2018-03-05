5207e4bc401eff92baafd4cdb053d8170fcaa8d9
blackenedgem <Chrisroberts97@btinternet.com>
2018-03-05 21:15:36 +0000

    Initial frontend interface with RMI to get something working

---
+++ src/FrontEnd.java
@@ -0,0 +1,31 @@
+import java.net.InetAddress;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class FrontEnd extends UnicastRemoteObject implements FrontEndInterface {
+    protected FrontEnd() throws RemoteException {
+    }
+
+    public static void main(String[] args) {
+        try {
+            FrontEnd obj = new FrontEnd();
+
+            System.out.println("Detected Local IP: " + InetAddress.getLocalHost().toString());
+            // Bind the remote object's stub in the registry
+            Registry register = LocateRegistry.createRegistry(1099);
+            register.rebind("FrontEnd", obj);
+
+            System.out.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String[] list() throws RemoteException {
+        return new String[0];
+    }
+}
