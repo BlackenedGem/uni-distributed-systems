03851d44b405e2b84e3d1d8f628049669606cd8b
blackenedgem <Chrisroberts97@btinternet.com>
2018-03-06 14:28:34 +0000

    Add list to server

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,30 @@
+import java.io.IOException;
 import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Server extends UnicastRemoteObject implements FrontEndInterface {
-    private Server() throws RemoteException {
+public class Server extends UnicastRemoteObject implements ServerInterface {
+    private static String BASE_DIR = "server_files_";
+
+    private int id;
+    private String FILES_DIR;
+
+    private Server(int id) throws RemoteException {
+        this.id = id;
+        this.FILES_DIR = BASE_DIR + id + "/";
     }
 
     public static void main(String[] args) {
         int serverID = 1;
 
         try {
-            Server obj = new Server();
+            Server obj = new Server(serverID);
 
             // System.out.println("Detected Local IP: " + InetAddress.getLocalHost().toString());
             // Bind the remote object's stub in the registry
@@ -27,7 +39,27 @@ public class Server extends UnicastRemoteObject implements FrontEndInterface {
     }
 
     @Override
-    public String[] list() {
-        return new String[]{"Hello", " ", "World", "!"};
+    public List<String> list() {
+        log("Sending listings to client");
+        List<String> listings = new ArrayList<>();
+
+        // Get listings by traversing through source directory
+        try {
+            Files.walk(Paths.get(FILES_DIR))
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        String listing = path.toString().substring(Server.BASE_DIR.length());
+                        listings.add(listing);
+                    });
+        } catch (IOException e) {
+            log("Could not find file listings: " + e.getMessage());
+            return new ArrayList<>();
+        }
+
+        return listings;
+    }
+
+    private void log(String msg) {
+        System.out.println(msg);
     }
 }
--- src/ServerInterface.java
+++ src/ServerInterface.java
@@ -1,6 +1,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ServerInterface extends Remote {
-    String[] list() throws RemoteException;
+    List<String> list() throws RemoteException;
 }
